5067a5a353be76aec5bc200dcd8c7796f74ffb04
Roman Damborsky <rdamborsky@gmail.com>
2017-05-31 13:30:49 +0200

    Initial with one star hardcoded

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	float width = 20f;
+	float height = 20f;
+
+	float initialDistance = 50f;
+
+	GameObject star;
+
+	void Start() {
+		GameObject Star = Resources.Load("Prefabs/Star", typeof(GameObject)) as GameObject;
+
+		star = Instantiate(Star);
+
+		Vector3 position = new Vector3(
+			              Random.Range(-width / 2, width / 2),
+			              Random.Range(-height / 2, height / 2),
+			              initialDistance);
+		star.GetComponent<StarScript>().Init(position);
+
+		star.SetActive(true);
+	}
+
+	void Update() {
+		
+	}
+}
---
+++ Assets/Scripts/StarScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarScript : MonoBehaviour {
+
+	public void Init(Vector3 position) {
+		transform.position = position;
+	}
+		
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
